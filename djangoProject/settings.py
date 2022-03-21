09af3971d962bc3536cabf32205f273b3ce3440e
kvelayutham1 <kvelayutham@unomaha.edu>
2022-03-21 11:40:53 -0500

    Initial commit with models for Project and Manager
    Added Assignment, Client, Employee tables

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -83,7 +83,8 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+#        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
